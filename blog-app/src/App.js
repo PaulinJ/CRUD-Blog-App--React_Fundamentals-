f399fdaad10ed2136dfcf3f3cd080c334d898d73
Paulin Junior C. <cingombapaulin@gmail.com>
2026-02-03 23:25:34 +0200

    dynamic values in templates

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Likes: {likes}</p>
+
+        <a href={link}>Google</a>
       </div>
 
     </div>
