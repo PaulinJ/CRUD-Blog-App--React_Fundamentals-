59371573bebc5edbf25c876e0b43b41830af61b3
Paulin Junior C. <cingombapaulin@gmail.com>
2026-02-04 09:05:18 +0200

    JS Map method in React

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,27 +1,20 @@
 import { useState } from "react";
    
 const Home = () => {
-    const [name, setName] = useState("Paul");
-
-    const handleClick = () => {
-        if(name === "Paul"){
-            setName("Junior");
-        }
-        else {
-            setName("Paul");
-        }
-        
-    }
+    const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
    
-
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click here</button>
-
-
-
+           {blogs.map((blog) => ( 
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by: {blog.author}</p>
+            </div>
+           )) }
         </div>
      );
 }
