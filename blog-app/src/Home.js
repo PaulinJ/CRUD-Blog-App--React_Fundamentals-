4a7116f45bde9dd72eaf4adc0d600e49c8c40cc9
Paulin Junior C. <cingombapaulin@gmail.com>
2026-02-04 08:05:54 +0200

    click events

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('hello there!');
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click here</button>
 
         </div>
      );
