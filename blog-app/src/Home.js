6d5d698a3b739f066f19d112fe2fddca5a13f32d
Paulin Junior C. <cingombapaulin@gmail.com>
2026-02-04 08:13:39 +0200

    event handling with parameterized function

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -2,10 +2,16 @@ const Home = () => {
     const handleClick = () => {
         console.log('hello there!');
     }
+    //function with param
+    const handleClickAgain = (name) => {
+        console.log("hello " + name);
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click here</button>
+            {/*how to reference a parameterised function: use an anonymous function and pass it in*/}
+            <button onClick={() => {handleClickAgain("Junior")}}>Click here Again</button>
 
         </div>
      );
