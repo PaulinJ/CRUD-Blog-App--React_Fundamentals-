7ab2b4f6712051c2dced8e8b135bcc7a71026afd
Paulin Junior C. <cingombapaulin@gmail.com>
2026-02-07 01:24:16 +0200

    React router dom and Switch element

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,18 +1,33 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+
 function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
   const link = "https://www.google.com";
 
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/Create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+
       </div>
 
-    </div>
+    </Router>
+
+    
   );
 }
 
